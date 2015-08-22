19821e1697d3d5b5afdf0ce0be70f07fe152d530
dmemes <willsmith0123456789@gmail.com>
2015-08-22 14:08:25 -0400

    Fix user search function

--- api/searchuser.php
+++ api/searchuser.php
@@ -13,7 +13,7 @@
 	
 	// stupid mysql requires the concat thing
 	// using php to add the % doesn't work
-	$query = "SELECT `username`, `userid` FROM `cgbstats_user` WHERE `username` LIKE CONCAT('%', ?, '%')";
+	$query = "SELECT `username`, `userid` FROM `cgbstats_user` WHERE `username` COLLATE UTF8_GENERAL_CI LIKE CONCAT('%', ?, '%')";
 	$res = $CGBStats->database->query($query, array($username));
 	echo json_encode($res);
 ?>
